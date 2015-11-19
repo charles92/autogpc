1ac5bf22629b56edc28592d4ceb9e34fbb4c4c57
Qiurui He <qh219@cam.ac.uk>
2015-11-19 01:07:35 +0000

    Implemented 2-D GPC plot functionality.

--- gpcplot.py
+++ gpcplot.py
@@ -4,12 +4,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from GPy.plotting.matplot_dep.base_plots import x_frame1D
+from GPy.plotting.matplot_dep.base_plots import x_frame1D, x_frame2D
 
 # Figure DPI
-default_dpi = 1200
+default_dpi = 300
+
 # Number of samples drawn from the posterior GP, which are then plotted
-default_res = 500
+default_res = 256
+
+# Number of contour levels
+default_lvl = 7
 
 class GPCPlot(object):
     """
@@ -53,19 +57,20 @@ class GPCPlot1D(GPCPlot):
         GPCPlot.__init__(self, model, dpi)
 
     def draw(self):
-        print 'TODO: draw 1D'
         m = self.model
         fig, ax = plt.subplots()
 
+        plots = {}
+
         # Data range
         xnew, xmin, xmax = x_frame1D(m.X, resolution=default_res)
-
-        # Data points
-        ax.plot(m.X, m.Y, label='Training data', linestyle='',
-            marker='x', mfc='blue', mew=1)
         ax.set_xlim(xmin, xmax)
         ax.set_ylim(ymin=-0.2, ymax=1.2)
         ax.set_yticks([0, 0.5, 1])
+
+        # Data points
+        plots['data'] = ax.plot(m.X, m.Y, label='Training data', linestyle='',
+            marker='x', mfc='blue', mew=1)
 
         # Latent function
         mu, var = m._raw_predict(xnew)
@@ -73,9 +78,10 @@ class GPCPlot1D(GPCPlot):
         lower = m.likelihood.gp_link.transf(mu - 2 * stdev)
         upper = m.likelihood.gp_link.transf(mu + 2 * stdev)
         mu = m.likelihood.gp_link.transf(mu)
-        plotGP(xnew, mu, lower=lower, upper=upper, ax=ax)
+        plots['link'] = plotGP(xnew, mu, lower=lower, upper=upper, ax=ax)
 
         self.fig = fig
+        return plots
 
 
 class GPCPlot2D(GPCPlot):
@@ -88,7 +94,51 @@ class GPCPlot2D(GPCPlot):
         GPCPlot.__init__(self, model, dpi)
 
     def draw(self):
-        print 'TODO: 2D'
+        m = self.model
+        fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
+
+        plots = {}
+
+        # Data range
+        xnew, _, _, xmin, xmax = x_frame2D(m.X, resolution=default_res)
+        ax0.set_xlim(xmin[0], xmax[0])
+        ax0.set_ylim(xmin[1], xmax[1])
+
+        # Data points
+        plots['data1'] = ax0.scatter(m.X[:,0], m.X[:,1], c=m.Y, marker='o',
+            edgecolors='none', alpha=0.5, vmin=-0.2, vmax=1.2, cmap=plt.cm.jet)
+        plots['data2'] = ax1.scatter(m.X[:,0], m.X[:,1], c=m.Y, marker='o',
+            edgecolors='none', alpha=0.5, vmin=-0.2, vmax=1.2, cmap=plt.cm.jet)
+
+        # Latent function
+        x0range = np.linspace(xmin[0], xmax[0], default_res)
+        x1range = np.linspace(xmin[1], xmax[1], default_res)
+        mu, var = m._raw_predict(xnew)
+
+        # Latent function - mean
+        mu = mu.reshape(default_res, default_res).T
+        cs = ax0.contour(x0range, x1range, mu, default_lvl,
+            vmin=mu.min(), vmax=mu.max(), cmap=plt.cm.jet)
+        # Make zero contour thicker
+        if np.all(cs.levels != 0):
+            cs.levels = np.hstack((cs.levels, 0))
+        zcind = np.where(cs.levels == 0)[0].flatten()
+        plt.setp(cs.collections[zcind], linewidth=2)
+        # Add contour labels
+        ax0.clabel(cs, fontsize='smaller')
+        plots['gpmu'] = cs
+
+        # Latent function - standard deviation
+        var = var.reshape(default_res, default_res).T
+        sd = np.sqrt(var)
+        cs = ax1.contour(x0range, x1range, sd, default_lvl // 2,
+            vmin=sd.min(), vmax=sd.max(), cmap=plt.cm.OrRd)
+        # Add contour labels
+        ax1.clabel(cs, fontsize='smaller')
+        plots['gpsd'] = cs
+
+        self.fig = fig
+        return plots
 
 
 class GPCPlot3D(GPCPlot):
